258bd2e943e4744d82139e2ed3a8dcfb8c6ad80b
John <johnfish218@gmail.com>
2019-03-08 14:00:23 -0600

    made throw alert more generic and added clear util

--- Lab 2 - Connect the Dots/src/bretzj/Util.java
+++ Lab 2 - Connect the Dots/src/bretzj/Util.java
@@ -7,24 +7,31 @@
  */
 package bretzj;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Dialog;
 
 /**
  * Utility class for helper methods
  */
 public class Util {
 
-    /**
-     * Creates an Alert
-     * @param header the header text
-     * @param content the content text
-     * @return the alert
-     */
-    public static Alert throwAlert(String header, String content) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setHeaderText(header);
-        alert.setContentText(content);
+    public static Dialog throwAlert(Dialog type, String title, String header, String content) {
+        type.setTitle(title);
+        type.setHeaderText(header);
+        type.setContentText(content);
 
-        return alert;
+        return type;
+    }
+
+    public static double dist(double x1, double y1, double x2, double y2) {
+        double dx = x1 - x2;
+        double dy = y1 - y2;
+
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public static void clearCanvas(Canvas canvas) {
+        canvas.getGraphicsContext2D().clearRect(0, 0, Main.WIDTH, Main.HEIGHT);
     }
 }
