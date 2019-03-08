2fd8961321c884ab1d058781f7e58d38f556ab95
John <johnfish218@gmail.com>
2019-03-08 09:02:25 -0600

    taylorize will make every submital it can

--- _Taylorize/taylorize/taylorize.py
+++ _Taylorize/taylorize/taylorize.py
@@ -2,41 +2,49 @@ import os
 import zipfile
 from pyfiglet import Figlet
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 def zipDir(path, ziph):
-   for root, dirs, files in os.walk(path):
-      for file in files:
-         if root.count('src') > 0 and (file.endswith('.java') or file.endswith('.fxml')):
-            file_path = os.path.join(root, file)
-            ziph.write(file_path, 'bretz/'+os.path.basename(file_path))
-         elif file.endswith('.jar'):
-            file_path = os.path.join(root, file)
-            ziph.write(file_path, os.path.basename(file_path))
+    hadJar = False
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if root.count('src') > 0 and (file.endswith('.java') or file.endswith('.fxml')):
+                file_path = os.path.join(root, file)
+                ziph.write(file_path, 'bretz/'+os.path.basename(file_path))
+            elif file.endswith('.jar'):
+                file_path = os.path.join(root, file)
+                ziph.write(file_path, os.path.basename(file_path))
+                hadJar = True
 
+    return hadJar
+                
 def main():
-   print(Figlet(font='slant').renderText('taylorize'))
+    print('Generating Submitalls...')
 
-   if not os.path.exists("_Submitals"):
-      os.mkdir("_Submitals")
+    if not os.path.exists('_Submitals'):
+        os.mkdir('_Submitals')
 
-   while True:
-      project = input("What is the project's name\n")
+    projects = [i for i in next(os.walk('.'))[1] if i.lower().count('lab')>0]
+    missingJars = 0
 
-      try:
-         project = [i for i in next(os.walk('.'))[1] if i.lower().count('lab')>0][int(project)]
-      except:
-         pass
-      
-      if os.path.exists(project):
-         print('Creating zip...')
+    printProgressBar(0, len(projects), prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-         zipf = zipfile.ZipFile(f"_Submitals/{project}.zip", 'w', zipfile.ZIP_DEFLATED)
-         zipDir(project, zipf)
-         zipf.close()
+    for i, project in enumerate(projects):
+        zipf = zipfile.ZipFile(f"_Submitals/{project}.zip", 'w', zipfile.ZIP_DEFLATED)
+        if not zipDir(project, zipf):
+            missingJars += 1
+        zipf.close()
+        printProgressBar(i + 1, len(projects), prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-         print('Zip file created')
-         break
-      else:
-         print(f"Valid projects are: {[i for i in next(os.walk('.'))[1] if i.lower().count('lab')>0]}")
+    print(f"{missingJars}/{len(projects)} projects are missing jars")
+
 
 if __name__ == "__main__":
-   main()
+    main()
