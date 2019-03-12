d9924b426906038a55509a48e1eda925cab6b8a6
John <johnfish218@gmail.com>
2019-03-12 13:09:41 -0500

    work on timing

--- Lab 3 - Connect the Dots Revisited/src/bretzj/Controller.java
+++ Lab 3 - Connect the Dots Revisited/src/bretzj/Controller.java
@@ -11,15 +11,13 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.TextInputDialog;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Optional;
+import java.util.LinkedList;
 
 import static bretzj.Util.clearCanvas;
 import static bretzj.Util.throwAlert;
@@ -43,6 +41,8 @@ public class Controller {
      * Created using dots from `container` to manipulate the canvas
      */
     private Picture picture;
+
+    private static boolean useLinkedListInstead = false;
 
     /**
      * Closes the window
@@ -122,51 +122,112 @@ public class Controller {
     }
 
     /**
-     * Opens the remove dots dialog
+     * Removes dots using index based methods
      */
     @FXML
     void removeDots() {
-        Dialog dialog = Util.throwAlert(new TextInputDialog(""), "Dot Remover", "Dot Remover",
-                "How many dots do you want to stay");
-        Optional result = dialog.showAndWait();
+        picture = Util.createPicture(container, useLinkedListInstead);
 
-        int number;
-        try {
-            number = (int) Double.parseDouble((String) result.get());
-            if (number < 3) {
-                Util.throwAlert(new Alert(Alert.AlertType.ERROR), "Error", "Invalid Number",
-                        "Number must be greater than or equal to 3.").show();
-                number = container.getDots().size();
-            }
-        } catch (NumberFormatException ignored) {
-            Util.throwAlert(new Alert(Alert.AlertType.ERROR), "Error", "Invalid Number",
-                    "Number must be greater than or equal to 3.").show();
-            number = container.getDots().size();
-        }
-
-        picture = new Picture(container, new ArrayList<>());
-
-        System.out.println("Size: " + picture.getDots().size());
-
-        picture.removeDots(number);
+        picture.removeDots(Picture.removeDotsStart(container), false);
         clearCanvas(canvas);
         picture.drawDots(canvas);
         picture.drawLines(canvas);
-
-        System.out.println("Size: " + picture.getDots().size());
     }
 
-//    /**
-//     * Initializes things for testing purposes
-//     */
-//    @FXML
-//    void initialize() {
-//        try {
-//            container.load(new File("dove.dot").toPath());
-//            picture = new Picture(container, new ArrayList<>());
-//            picture.drawDots(canvas);
-//            picture.drawLines(canvas);
-//        } catch (IOException ignored) {
-//        }
-//    }
+    /**
+     * Removes dots using an iterator
+     */
+    @FXML
+    void removeDotsIterator() {
+        picture = Util.createPicture(container, useLinkedListInstead);
+
+        picture.removeDots(Picture.removeDotsStart(container), true);
+        clearCanvas(canvas);
+        picture.drawDots(canvas);
+        picture.drawLines(canvas);
+    }
+
+    /**
+     * Use LinkedLists instead of ArrayLists for constructors
+     */
+    @FXML
+    void useLinked() {
+        useLinkedListInstead = !useLinkedListInstead;
+        System.out.println("useLinkedListInstead = " + useLinkedListInstead);
+    }
+
+    /**
+     * Initializes things for testing purposes
+     */
+    @FXML
+    void initialize() {
+        try {
+            container.load(new File("balloon.dot").toPath());
+            picture = new Picture(container, new ArrayList<>());
+            picture.drawLines(canvas);
+            picture.drawDots(canvas);
+        } catch (IOException ignored) {
+        }
+    }
+
+    private void testPicture(Picture p, int dots, String name) {
+        long start, end;
+        StringBuilder output = new StringBuilder();
+        Picture[] tests = {
+                new Picture(p, new ArrayList<>()),
+                new Picture(p, new LinkedList<>()),
+                new Picture(p, new ArrayList<>()),
+                new Picture(p, new LinkedList<>())
+        };
+
+        for (int i = 0; i < tests.length; i++) {
+            start = System.currentTimeMillis();
+            if (i < 2) { // index
+                tests[i].removeDots(dots, false);
+            } else { // iterator
+                tests[i].removeDots(dots, true);
+            }
+            end = System.currentTimeMillis();
+
+            switch (i) {
+                case 0:
+                    output.append("\tIndexed ArrayList:   ").append(formatTime(end - start)).append("\n");
+                    break;
+                case 1:
+                    output.append("\tIndexed LinkedList:  ").append(formatTime(end - start)).append("\n");
+                    break;
+                case 2:
+                    output.append("\tIterated ArrayList:  ").append(formatTime(end - start)).append("\n");
+                    break;
+                case 3:
+                    output.append("\tIterated LinkedList: ").append(formatTime(end - start)).append("\n");
+                    break;
+            }
+        }
+        throwAlert(new Alert(Alert.AlertType.INFORMATION), "Test Result", name, output.toString()).showAndWait();
+    }
+
+    private String formatTime(long millis) {
+        long remain = millis % 1000;
+        millis = (millis - remain) / 1000;
+        long seconds = millis % 60;
+        millis = (millis - seconds) / 60;
+        long minutes = millis % 60;
+        long hours = (millis - minutes) / 60;
+        return String.format("%02d:%02d:%02d:%03d", hours, minutes, seconds, remain);
+    }
+
+    public void runTests() throws IOException {
+        Picture balloon100 = new Picture(new ArrayList<>());
+        Picture balloon1000 = new Picture(new ArrayList<>());
+        Picture skull = new Picture(new ArrayList<>());
+
+        balloon100.load(new File("balloon.dot").toPath());
+        balloon1000.load(new File("balloon.dot").toPath());
+        skull.load(new File("skull.dot").toPath());
+
+        testPicture(balloon100, 100, "Balloon100");
+        testPicture(balloon100, 1000, "Balloon1000");
+        testPicture(skull, 9000, "Skull9000");
+    }
 }
