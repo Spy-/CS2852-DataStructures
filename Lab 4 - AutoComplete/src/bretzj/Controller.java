e276d1726ae4f3f4f87cd24aa84f3a1bee05b471
John <johnfish218@gmail.com>
2019-03-16 12:29:01 -0500

    Start of lab 4

---
+++ Lab 4 - AutoComplete/src/bretzj/Controller.java
@@ -0,0 +1,54 @@
+package bretzj;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+
+    @FXML
+    private TextArea Matches;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    private Label matchesCount;
+
+    @FXML
+    void arrayListEnhanced(ActionEvent event) {
+
+    }
+
+    @FXML
+    void arrayListIndex(ActionEvent event) {
+
+    }
+
+    @FXML
+    void linkedListEnhanced(ActionEvent event) {
+
+    }
+
+    @FXML
+    void linkedListIndex(ActionEvent event) {
+
+    }
+
+    @FXML
+    void open(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchUpdate(KeyEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ Lab 4 - AutoComplete/src/bretzj/Main.java
@@ -0,0 +1,23 @@
+package bretzj;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("autocomplete.fxml"));
+        primaryStage.setTitle("Auto Complete");
+        primaryStage.setScene(new Scene(root, 400, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
