7d9f56191c4e6929d8fbf51f1885917d64c1985e
John <johnfish218@gmail.com>
2019-04-02 11:52:08 -0500

    Added more tests for BufferedInputStream

--- Lab 5 - Buffered IO/src/bretzj/BufferedInputStreamTest.java
+++ Lab 5 - Buffered IO/src/bretzj/BufferedInputStreamTest.java
@@ -10,16 +10,27 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Tests for BufferedInputStream's
+ */
 class BufferedInputStreamTest {
 
     private BufferedInputStream input;
     private static final byte[] BYTES = new byte[]{1, 2, 4, 8, 16, 32, 64};
 
+    /**
+     * Called before each test
+     */
     @BeforeEach
     void setUp() {
         input = new BufferedInputStream(new ByteArrayInputStream(BYTES), BYTES.length);
     }
 
+    /**
+     * Read until the end of file
+     *
+     * @throws IOException some exception
+     */
     @Test
     void read() throws IOException {
         assertEquals(1, input.read());
@@ -32,6 +43,11 @@ class BufferedInputStreamTest {
         assertEquals(-1, input.read());
     }
 
+    /**
+     * Have read values stored into a array that is smaller than the buffered array
+     *
+     * @throws IOException some exception
+     */
     @Test
     void readAndCopyToSmallArray() throws IOException {
         byte[] underSized = new byte[6];
@@ -39,6 +55,11 @@ class BufferedInputStreamTest {
         assertArrayEquals(new byte[]{1, 2, 4, 8, 16, 32}, underSized);
     }
 
+    /**
+     * Have read values stored into an array that is bigger than the buffered array
+     *
+     * @throws IOException some exception
+     */
     @Test
     void readAndCopyToBigArray() throws IOException {
         byte[] overSized = new byte[12];
@@ -46,6 +67,24 @@ class BufferedInputStreamTest {
         assertArrayEquals(new byte[]{1, 2, 4, 8, 16, 32, 64, 0, 0, 0, 0, 0}, overSized);
     }
 
+    /**
+     * Have read values stored into an array after an arbitrary number of .read()'s
+     *
+     * @throws IOException some exception
+     */
+    @Test
+    void readAndCopyAfterRead() throws IOException {
+        byte[] array = new byte[8];
+        input.read();
+        input.read();
+        assertEquals(5, input.read(array));
+        assertArrayEquals(new byte[]{4, 8, 16, 32, 64, 0, 0, 0}, array);
+    }
+
+    /**
+     * Tests if bits are read properly and the proper errors are thrown if not enough bits are read
+     * @throws IOException some exception
+     */
     @Test
     void readBit() throws IOException {
         // read a whole byte
@@ -60,7 +99,8 @@ class BufferedInputStreamTest {
 
         // test calling read() after an invalid number of bits read
         input.readBit();
-        assertThrows(IllegalStateException.class, input::read);
+        assertThrows(IllegalStateException.class, input::read); // read(int)
+        assertThrows(IllegalStateException.class, input::read); // read(byte[])
 
         // finish reading bits and try to read again
         input.readBit();
